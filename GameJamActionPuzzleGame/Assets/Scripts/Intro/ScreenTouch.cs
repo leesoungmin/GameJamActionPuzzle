a6d033b5ec583bc2506bbed6aa1fdc8ebe07b52a
leesoungmin <kkomaeng770@gmail.com>
2021-11-25 14:39:00 +0900

    FirstProgram

---
+++ GameJamActionPuzzleGame/Assets/Scripts/Intro/ScreenTouch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenTouch : MonoBehaviour
+{
+    public void TouchScreen()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
